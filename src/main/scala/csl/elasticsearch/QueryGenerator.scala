ac57ba12e06816d2db0e5bc5fa3f76b15f42f5b8
Kevin van Cleef <kevin@developmentstudio.nl>
2015-06-03 15:15:04 +0200

    Removes whitespace which is giving an error.

--- src/main/scala/csl/elasticsearch/QueryGenerator.scala
+++ src/main/scala/csl/elasticsearch/QueryGenerator.scala
@@ -15,7 +15,6 @@ class MatchQueryGenerator extends QueryGenerator {
       |       "must": [
     """.stripMargin +
       (generate(v.request, "request.") ++ generate(v.response, "response.")).map(_.toString).mkString(",") +
-
     """       ]
       |     }
       |   },
