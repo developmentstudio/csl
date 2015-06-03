d843f76f7950f6cefd247281e2ebd70b8349150f
Kevin van Cleef <kevin@developmentstudio.nl>
2015-06-03 15:15:55 +0200

    Adds/Refactored search of variables and storing it to a MySQL database.

--- src/main/scala/csl/elasticsearch/ResponseParser.scala
+++ src/main/scala/csl/elasticsearch/ResponseParser.scala
@@ -10,37 +10,38 @@ class ResponseParser
   def parseJSON(json: String): Response = {
     parse(json).extract[Response]
   }
+}
 
-  case class Response(_scroll_id: String, took: String, timed_out: Boolean, _shards: Shards, hits: Hits);
-  case class Shards(total: Int, successful: Int, failed: Int)
-  case class Hits(total: Int, max_score: Double, hits: List[Result])
-  case class Result(_index: String, _type: String, _id: String, _score: Double, _source: JObject)
+case class Response(_scroll_id: String, took: String, timed_out: Boolean, _shards: Shards, hits: Hits);
+case class Shards(total: Int, successful: Int, failed: Int)
+case class Hits(total: Int, max_score: Double, hits: List[Result])
+case class Result(_index: String, _type: String, _id: String, _score: Double, _source: JObject)
+{
+  var properties = Map[String, Any]()
+
+  def source(key: String): Option[Any] =
   {
-    var properties = Map[String, Any]()
+    flatten(this._source.values)
+    properties.get(key)
+  }
 
-    def source(key: String): Option[Any] =
-    {
-      flatten(this._source.values)
+  def sourceAsJson: String = compact(render(_source))
 
-      properties.get(key)
-    }
-
-    private def flattenProperty(key: String, value: Any, prefix: String = ""): Unit = {
-      value match {
-        case v: String => properties = properties + (prefix -> v)
-        case v: BigInt => properties = properties + (prefix -> v)
-        case v: Double => properties = properties + (prefix -> v)
-        case v: Map[_, _] => {
-          val m = v.asInstanceOf[Map[String, Any]]
-          flatten(m, prefix + ".")
-        }
-        case v => throw new Exception(s"This type in not implemented and can therefore not be flattened. Class: ${v.getClass} Value: ${v}")
+  private def flattenProperty(key: String, value: Any, prefix: String = ""): Unit = {
+    value match {
+      case v: String => properties = properties + (prefix -> v)
+      case v: BigInt => properties = properties + (prefix -> v)
+      case v: Double => properties = properties + (prefix -> v)
+      case v: Map[_, _] => {
+        val m = v.asInstanceOf[Map[String, Any]]
+        flatten(m, prefix + ".")
       }
+      case v => throw new Exception(s"This type in not implemented and can therefore not be flattened. Class: ${v.getClass} Value: ${v}")
     }
-    private def flatten(m: Map[String, Any], prefix: String = ""): Unit = {
-      for {
-        (k, v) <- m
-      } flattenProperty(k, v, prefix + k)
-    }
+  }
+  private def flatten(m: Map[String, Any], prefix: String = ""): Unit = {
+    for {
+      (k, v) <- m
+    } flattenProperty(k, v, prefix + k)
   }
 }
--- src/main/scala/csl/elasticsearch/Search.scala
+++ src/main/scala/csl/elasticsearch/Search.scala
@@ -1,37 +1,26 @@
 package csl.elasticsearch
 
-import java.sql.PreparedStatement
-
-import csl.ast._
+import csl.ast.Variable
 import csl.storage.{connection => MySQLConnection}
-import org.json4s.JsonAST._
-import org.json4s.jackson.JsonMethods.{parse => parseJSON, _}
-import scala.collection.Map
 
-
+import java.sql.PreparedStatement
 import scala.concurrent.ExecutionContext.Implicits.global
-import wabisabi.{Scan, SearchUriParameters}
-
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
+import wabisabi.{Scan, SearchUriParameters}
+
 class ScrollSearch(variable: Variable, generator: FilterQueryGenerator = new FilterQueryGenerator)
 {
-  private var scrollId: String = ""
-  private val _index: String = "20141016"
   private val query: String = generator.generate(variable)
   private val responseParser: ResponseParser = new ResponseParser
 
-  def search(): Unit =  {
+  def search(_index: String = "20141016"): Unit =  {
     val req = client.search(_index, query, uriParameters = SearchUriParameters(searchType = Some(Scan), scroll = Some("10m")))
     req onComplete {
-      case Success(response) => {
-        val body = response.getResponseBody()
-
-        println(responseParser.parseJSON(body))
-
-        //val parsedBody = parseJSON(body)
-        //getDocuments(filterScrollId(parsedBody))
+      case Success(body) => {
+        val response = responseParser.parseJSON(body.getResponseBody())
+        scroll(response._scroll_id)
       }
       case Failure(e) => {
         println("An error has occured: " + e.getMessage)
@@ -39,62 +28,52 @@ class ScrollSearch(variable: Variable, generator: FilterQueryGenerator = new Fil
     }
   }
 
-//  private def getDocuments(scrollId: String): Unit = Future {
-//    client.scroll("10m", scrollId) onComplete {
-//      case Success(response) => {
-//        val body = response.getResponseBody()
-//        val parsedBody = parseJSON(body)
-//        val collection = createResultCollection(parsedBody \ "hits" \ "hits")
-//        if (collection.hasResults) {
-//          collection.store(variable.name)
-//          getDocuments(filterScrollId(parsedBody))
-//        } else {
-//          println("NO RESULT!")
-//        }
-//      }
-//      case Failure(e) => {
-//        println("An error has occured: " + e.getMessage)
-//      }
-//    }
-//  }
+  private def scroll(scroll_id: String): Unit = Future {
+    client.scroll("10m", scroll_id) onComplete {
+      case Success(body) => {
 
-  private def filterScrollId(json: JValue): String = (json \ "_scroll_id").values.toString()
+        val response = responseParser.parseJSON(body.getResponseBody())
 
-  private def setScrollId(newScrollId: String): Unit = scrollId = newScrollId
+        val collection = new ResultCollection(response.hits.hits)
+
+        if (collection.hasResults) {
+          collection.save(variable.name)
+          scroll(response._scroll_id)
+        } else {
+          println("No Results!")
+        }
+      }
+      case Failure(e) => {
+        println("An error has occured: " + e.getMessage)
+      }
+    }
+  }
 
 }
 
-//class ResultCollection(val results: List[Result] = List.empty) {
-//
-//  override def toString: String = s"ResultCollection(${results.size})"
-//
-//  def hasResults: Boolean = results.size > 0
-//
-//  def store(label: String = ""): Unit = {
-//    def addDoc(result: Result, statement: PreparedStatement) = {
-//      statement.setString(1, result._index)
-//      statement.setString(2, result._type)
-//      statement.setString(3, result._id)
-//      statement.setString(4, result._source)
-//      statement.setString(5, label)
-//      statement.addBatch()
-//    }
-//
-//    val sqlQuery = "INSERT INTO result (_index, _type, _id, _source, label) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE _id = _id"
-//    val statement = MySQLConnection.prepareStatement(sqlQuery)
-//    for {
-//      result <- results
-//    } addDoc(result, statement)
-//
-//
-//    statement.executeBatch()
-//    statement.close()
-//  }
-//
-//}
-//
-//class Result(val _index: String, val _type: String, val _id: String, val _source: String) {
-//
-//  override def toString: String = s"Result(${_index}, ${_type}, ${_id}, ${_source})"
-//
-//}
+class ResultCollection(val results: List[Result] = List.empty) {
+
+  override def toString: String = s"ResultCollection(${results.size})"
+
+  def hasResults: Boolean = results.size > 0
+
+  def save(label: String = ""): Unit = {
+    def addDoc(result: Result, statement: PreparedStatement) = {
+      statement.setString(1, result._index)
+      statement.setString(2, result._type)
+      statement.setString(3, result._id)
+      statement.setString(4, result.sourceAsJson)
+      statement.setString(5, label)
+      statement.addBatch()
+    }
+
+    val sqlQuery = "INSERT INTO result (_index, _type, _id, _source, label) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE _id = _id"
+    val statement = MySQLConnection.prepareStatement(sqlQuery)
+    for {
+      result <- results
+    } addDoc(result, statement)
+    statement.executeBatch()
+    statement.close()
+  }
+
+}
