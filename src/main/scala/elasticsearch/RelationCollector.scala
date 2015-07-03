9ddc343d37669b2dbb75ae9cb9cadf25bc64d111
Kevin van Cleef <kevin@developmentstudio.nl>
2015-07-03 14:15:25 +0200

    Cleanup

--- src/main/scala/elasticsearch/RelationCollector.scala
+++ src/main/scala/elasticsearch/RelationCollector.scala
@@ -10,8 +10,7 @@ import wabisabi.{Scan, SearchUriParameters}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
-class RelationCollector(detector: Detector)
-{
+class RelationCollector(detector: Detector) {
   val index: String = "20141016"
 
   private val generator: FilterQueryGenerator = new FilterQueryGenerator
@@ -21,57 +20,43 @@ class RelationCollector(detector: Detector)
   private val status = new CollectorStatus("relation_request_status.log")
 
   def collect: Boolean = {
-    println("Start Relation Collector!")
-    this.relations.distinct foreach(search)
+    this.relations.distinct foreach (search)
     this.waitForDocumentCollectionToComplete
-    println("Collection of Related Documents completed!") // TODO: Remove
     true
   }
 
-  private def search(relation: Relation): Unit =
-  {
+  private def search(relation: Relation): Unit = {
     if (relation.properties.length > 0 && this.detector.find.pattern.elements.length > 1) {
       val query = this.generator.generate(relation.properties)
-      try {
-        val request = client.search(index, query, uriParameters = SearchUriParameters(searchType = Some(Scan), scroll = Some("10m")))
-        request onComplete {
-          case Success(r) =>
-            val response = ResponseParser.parseJSON(r.getResponseBody)
-            this.scrollNextPage(response._scroll_id, relation)
-          case Failure(e) => println("An error has occured: " + e.getMessage)
-        }
-      } catch {
-        case e: Throwable => println("ERROR?!")
+      val request = client.search(index, query, uriParameters = SearchUriParameters(searchType = Some(Scan), scroll = Some("10m")))
+      request onComplete {
+        case Success(r) =>
+          val response = ResponseParser.parseJSON(r.getResponseBody)
+          this.scrollNextPage(response._scroll_id, relation)
+        case Failure(e) => println("An error has occured: " + e.getMessage)
       }
     } else {
       status.setCompleted(relation.toString)
     }
   }
 
-  private def scrollNextPage(scroll_id: String, relation: Relation): Unit =
-  {
-    try {
-      val request = client.scroll("10m", scroll_id)
-      request.onComplete {
-        case Success(r) =>
-          val response = ResponseParser.parseJSON(r.getResponseBody)
-          if (response.hasHits) {
-            Storage.save(response, None, this.detector.find.relation.keys)
-            this.scrollNextPage(response._scroll_id, relation)
-          } else {
-            status.setCompleted(relation.toString)
-          }
-        case Failure(e) => throw new Exception(e)
-      }
-    } catch {
-      case e: Throwable => println("?!RORRE")
+  private def scrollNextPage(scroll_id: String, relation: Relation): Unit = {
+    val request = client.scroll("10m", scroll_id)
+    request.onComplete {
+      case Success(r) =>
+        val response = ResponseParser.parseJSON(r.getResponseBody)
+        if (response.hasHits) {
+          Storage.save(response, None, this.detector.find.relation.keys)
+          this.scrollNextPage(response._scroll_id, relation)
+        } else {
+          status.setCompleted(relation.toString)
+        }
+      case Failure(e) => throw new Exception(e)
     }
-
   }
 
-  private def waitForDocumentCollectionToComplete: Unit =
-  {
-    val parts = this.relations map(_.toString)
+  private def waitForDocumentCollectionToComplete: Unit = {
+    val parts = this.relations map (_.toString)
     while (!this.status.isCompleted(parts)) {
       TimeUnit.SECONDS.sleep(1);
     }
--- src/main/scala/elasticsearch/interpreter/Interpreter.scala
+++ src/main/scala/elasticsearch/interpreter/Interpreter.scala
@@ -23,10 +23,10 @@ object Interpreter {
 
     val patternDetector = new PatternDetector(detector)
     val matchedDocuments = patternDetector.detect
-    println(patternDetector.totalMatches + " matches found existing of a total of " + matchedDocuments.length + " documents.")
+    println(patternDetector.totalMatches + " matches found existent of a total of " + matchedDocuments.length + " documents.")
 
 
-    // TODO: Handle result / export result (JSON, XML, CSV, ES-QUERY, HTML TABLE)
+    // TODO: Handle result / export result (CSV, ES-QUERY)
     matchedDocuments.foreach(d => println(d.source("request.cookies.Coolblue-Session").getOrElse("Property not found")))
 
 
