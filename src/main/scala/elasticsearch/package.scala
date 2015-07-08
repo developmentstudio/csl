ed1cf7af32b025feac83f299a4fe4df098ea579a
Kevin van Cleef <kevin@developmentstudio.nl>
2015-07-08 11:08:51 +0200

    Get elasticsearch hostname and port number from config file instead of hardcoded.

--- src/main/scala/elasticsearch/package.scala
+++ src/main/scala/elasticsearch/package.scala
@@ -4,6 +4,9 @@ package object elasticsearch {
 
   type Query = String
 
-  val client = new Client("http://localhost:9200")
+  private val hostname = Config.setting("settings.elasticsearch.hostname")
+  private val port = Config.setting("settings.elasticsearch.port")
+
+  val client = new Client(s"http://$hostname:$port")
 
 }
